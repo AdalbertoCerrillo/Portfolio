792b551cc818cd97ab7198cc17c9c6f9883d83d7
AdalbertoCV <34152734@uaz.edu.mx>
2025-09-06 11:12:38 -0600

    Update experience

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,5 +1,6 @@
 import labsolLogo from '../../images/Labsol.png'
 import freelance from '../../images/freelance.png'
+import radii from '../../images/radii.png'
 
 const Experience = () =>{
     const infoLabsol = `
@@ -9,7 +10,11 @@ const Experience = () =>{
     const infoFreelance = `I am always looking for development projects to provide my clients and employers with high-quality service. I strive to ensure that the products I deliver meet the highest standards. Since January 2025, I have been working on several projects, offering my services as a freelance programmer.
     `;
 
+    const infoRadii = `Radii Manufacturing is a startup focused on automating manufacturing processes to take projects from design to finished parts. With an AI-powered platform and a centralized network of manufacturers, Radii is emerging as one of the most promising startups in Latin America.`;
+
     const labsolLink = 'https://labsol.cozcyt.gob.mx/';
+
+    const radiiLink = 'https://www.radii.com.mx/es';
 
     return (
         <div className="main-card">
@@ -19,9 +24,14 @@ const Experience = () =>{
                 <a href={labsolLink} target="_blank" rel="noopener noreferrer"><img className='education-image' src={labsolLogo} alt='Labsol Logo'></img></a>
             </div>
             <div className="info-card">
-                <h3>Freelance Programmer (Jan 2025 - now)</h3>
+                <h3>Freelance Programmer (Jan 2025 - Jul 2025)</h3>
                 <p>{infoFreelance}</p>
                 <img className='education-image' src={freelance} alt='Freelance img'></img>
+            </div>
+            <div className="info-card">
+                <h3>Radii Manufacturing (Jul 2025 - now)</h3>
+                <p>{infoRadii}</p>
+                <a href={radiiLink} target="_blank" rel="noopener noreferrer"><img className='education-image' src={radii} alt='Radii logo'></img></a>
             </div>
         </div>
     );
